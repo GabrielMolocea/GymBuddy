5a2d553c0a28823f948669d45265f98bfe15c658
Gabriel Molocea <gabrielgabriel1235@gmail.com>
2023-04-02 14:33:07 +0300

    setup push

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import {Button, StyleSheet, Text, View} from 'react-native';
+
+export default function App() {
+  return (
+    <View className='flex-1 bg-red-500'>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
