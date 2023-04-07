ea2fe279aaa476e106d0c5afc6e06a24a328e2c4
Gabriel Molocea <gabrielgabriel1235@gmail.com>
2023-04-07 19:34:09 +0300

    add redux

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import {Text, View} from 'react-native';
+import { Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 export default function App(): JSX.Element {
   return (
-    <View className='flex-1 bg-red-500'>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View className='flex-1 bg-red-500'>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
