dae9c7dff0a9d92a93acf96bc9858c8bcacb1a0f
Gabriel Molocea <gabrielgabriel1235@gmail.com>
2023-04-16 13:31:41 +0300

    simpify and change the bottom navigations

--- App.tsx
+++ App.tsx
@@ -1,42 +1,89 @@
 import React from 'react';
 import { store } from './redux/store/store';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator, BottomTabNavigationOptions, BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
 
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, RouteProp } from '@react-navigation/native';
 import { Provider } from 'react-redux';
 import { Ionicons } from '@expo/vector-icons';
 
 import HomeScreen from './components/Screens/HomeScreen';
-import ProfileScreen from './components/Screens/ProfieScreen';
+import ProfileScreen from './components/Screens/ProfileScreen';
+import TrainingListScreen from './components/Screens/TrainingListScreen';
+import AIChatScreen from './components/Screens/AIChatScreen';
+import DietsScreen from './components/Screens/DietsScreen';
 
 const Tab = createBottomTabNavigator();
+
+type Tab = {
+  name: string;
+  component: React.ComponentType<any>;
+  icon: string;
+};
+
+const tabs: Tab[] = [
+  {
+    name: 'Home',
+    component: HomeScreen,
+    icon: 'home-outline',
+  },
+  {
+    name: 'Training List',
+    component: TrainingListScreen,
+    icon: 'barbell-outline',
+  },
+  {
+    name: 'A.I Chat',
+    component: AIChatScreen,
+    icon: 'globe-outline',
+  },
+  {
+    name: 'Diets',
+    component: DietsScreen,
+    icon: 'leaf-outline',
+  },
+  {
+    name: 'Profile',
+    component: ProfileScreen,
+    icon: 'person-outline',
+  },
+];
+
+type BottomTabParamList = {
+  [key: string]: undefined;
+};
+
+type Props = {
+  navigation: BottomTabNavigationProp<BottomTabParamList>;
+  route: RouteProp<BottomTabParamList, string>;
+};
 
 export default function App(): JSX.Element {
   return (
     <Provider store={store}>
       <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen
-            name='Home'
-            component={HomeScreen}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <Ionicons name="home-outline" size={size} color={color} />
-              ),
-            }} />
-          <Tab.Screen
-            name='Profile'
-            component={ProfileScreen}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <Ionicons name="person-outline" size={size} color={color} />
-              ),
-            }} />
+          {tabs.map((tab: Tab) => (
+            <Tab.Screen
+              key={tab.name}
+              name={tab.name}
+              component={tab.component}
+              options={({ route, navigation }) => ({
+                tabBarIcon: ({ color }) => (
+                  <Ionicons
+                    name={tab.icon}
+                    size={23}
+                    color={route.name === tab.name ? 'red' : color}
+                  />
+                ),
+                tabBarLabelStyle: {
+                  color: route.name === tab.name ? 'red' : '#333',
+                },
+              })}
+            />
+          ))}
         </Tab.Navigator>
-
       </NavigationContainer>
     </Provider>
-
   );
 }
 
---
+++ components/Screens/DietsScreen.tsx
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+
+const DietsScreen = (): JSX.Element => {
+    return (
+        <View className='flex-1 bg-red-500'>
+            <Text>Profile Pic</Text>
+            <StatusBar style="auto" />
+        </View>
+    )
+}
+
+export default DietsScreen
